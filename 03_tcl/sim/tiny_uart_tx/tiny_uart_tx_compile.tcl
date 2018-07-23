6fddbb63a0e634324137fde1a1b7431a54c8ea05
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2018-07-23 21:51:28 +0200

    tiny_uart_tx_compile.tcl -> added compile script

---
+++ 03_tcl/sim/tiny_uart_tx/tiny_uart_tx_compile.tcl
@@ -0,0 +1,36 @@
+##************************************************************************
+## @copyright   LGPLv3
+## @author      akae
+##
+## @file        tiny_uart_tx_compile.tcl
+##
+## @brief       compile script
+## @details
+##
+## @date        2018-07-23
+## @version     0.1
+##************************************************************************
+
+
+
+# path setting
+#
+set path_tb "../02_tb"
+set path_src "../01_src"
+#
+
+
+
+# Compile Design
+#
+vcom -93 $path_src/tiny_uart_sfr.vhd
+vcom -93 $path_src/tiny_uart_cntr.vhd
+vcom -93 $path_src/tiny_uart_parity.vhd
+vcom -93 $path_src/tiny_uart_tx.vhd
+#
+
+
+# Compile TB
+#
+vcom -93 $path_tb/tiny_uart_tx_tb.vhd
+#
