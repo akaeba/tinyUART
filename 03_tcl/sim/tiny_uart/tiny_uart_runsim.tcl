b6ce775be6750c2abb1a102a0707b2f9f30392a3
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2018-08-09 19:49:09 +0200

    added sim script

---
+++ 03_tcl/sim/tiny_uart/tiny_uart_runsim.tcl
@@ -0,0 +1,24 @@
+##************************************************************************
+## @copyright   LGPLv3
+## @author      akae
+##
+## @file        tiny_uart_runsim.tcl
+## @note
+##
+## @brief       starts simulation
+## @details
+##
+## @date        2018-08-09
+## @version     0.1
+##************************************************************************
+
+
+
+# start simulation, disable optimization
+vsim -novopt -gDO_ALL_TEST=true -t 1ps work.tiny_uart_tb
+
+# load Waveform
+do "../03_tcl/sim/tiny_uart/tiny_uart_waveform.do"
+
+# sim until finish
+run 50 ms
